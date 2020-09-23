98d207c586e22cce8ca1d294e94497b1181f907b
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-09-23 13:50:38 +0530

    added web support

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:encryptor/utils/Constants.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './Encryptor.dart';
 import 'package:flutter/services.dart';
 
 Future main() async {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
